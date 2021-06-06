c7188c6ffca5e05ac1fbf92dbaed1077938a4162
takamatsuryota3 <tj.ryota3@gmail.com>
2021-06-06 18:58:14 +0900

    数値と文字列、日本語の表示

---
+++ Sample.java
@@ -0,0 +1,15 @@
+public class Sample{
+    public static void main (String[] args){
+        // 数値の表示
+        System.out.print(123);
+        System.out.println(456);
+
+        // 文字列の表示
+        System.out.print("ABC");
+        System.out.println("DEF");
+
+        // 日本語を表示
+        System.out.print("日本語を");
+        System.out.println("表示");
+    }
+}
