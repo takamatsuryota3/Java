b511a7d9df941997baa96a6ff238ab12fc7707b1
RyotaTakamatsu <tj.ryota3@gmail.com>
2022-11-28 22:40:48 +0900

    以下の内容を実装 ・くじ引き ・カードの番号穴あけ ・カード再表示

--- Study/Bingo.java
+++ Study/Bingo.java
@@ -14,15 +14,12 @@ import java.util.Collections;
 public class Bingo {
     public static final String LINE_SEPARATOR = System.getProperty("line.separator");   // 改行コード
     public static void main(String[] args) {
-        // カードの準備
+        // 1. カードの準備
         int[][] card = new int[5][5];
-        ArrayList<Integer> list = new ArrayList<Integer>();
-
+        ArrayList<Integer> list = new ArrayList<>();
         for (int i = 1; i <99; i++){
             list.add(i);
         }
-
-        // リストをシャッフル
         Collections.shuffle(list);
         
         int n = 0;
@@ -36,22 +33,66 @@ public class Bingo {
         // カードのタイトル
         System.out.println("   B |  I  |  N  |  G  |  O");
 
-        //　カード部分
-        for (int row = 0; row < 5; row++){
-            for (int cell = 0; cell < 5; cell++){
+        //　2. カードの表示
+        for (int j = 0; j < 5; j++){
+            for (int i = 0; i < 5; i++){
                 // ど真ん中は最初から空けておく
-                if (row == 2 && cell == 2){
+                if (i == 2 && j == 2){
                     System.out.print("  ☆  |");
                 }else{
-                    System.out.printf("%3d",card[cell][row]);
+                    System.out.printf("%3d",card[j][i]);
 
                     // 5列目の後ろは縦線つけない
-                    if (cell != 4){
+                    if (i != 4){
                         System.out.print("  |");
                     }
                 }
             }
             System.out.println(LINE_SEPARATOR);
+        }
+
+        for (int bingoNum = 1; bingoNum <= 10; bingoNum++){
+            // 3. くじを引き、番号を表示
+            Collections.shuffle(list);
+            int number = list.get(bingoNum);
+            System.out.println(bingoNum + "回目：" + number);
+
+            // 4. 表示された番号が、カードの中にあれば、番号部分を穴あけ
+            for ( int j = 0; j < 5; j++){
+                for (int i = 0; i < 5; i++){
+                    if(card[j][i] == number){
+                        card[j][i] = 0;
+                    }
+                }
+            }
+
+            // カードのタイトル
+            System.out.println("   B |  I  |  N  |  G  |  O");
+
+            // 5. カードの表示
+            for (int j = 0; j < 5; j++){
+                for (int i = 0; i < 5; i++){
+                    // ど真ん中は最初から空けておく
+                    if (i == 2 && j == 2){
+                        System.out.print("  ☆  |");
+                    }else if (card[j][i] == 0){
+                        if (i != 4){
+                            System.out.print("  ●  |");
+                        }else{
+                            System.out.print("  ●  ");
+                        }
+                    }
+                    else{
+                        System.out.printf("%3d",card[j][i]);
+
+                        // 5列目の後ろは縦線つけない
+                        if (i != 4){
+                            System.out.print("  |");
+                        }
+                    }
+                }
+                System.out.println(LINE_SEPARATOR);
+            }
         }
     }
 }
